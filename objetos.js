6f7c4ca2333389b39f87a3ff928bbb2eb3ea3287
TonyDev92 <tonydeveloper92@gmail.com>
2023-01-27 11:29:14 +0100

    Ejercicio 2 de JavaScript

---
+++ objetos.js
@@ -0,0 +1,12 @@
+const misDatos = {
+    nombre: "Tony",
+    edad : 30,
+    developer : true,
+    fechaDeNacimiento : new Date (sep, 03 , 1992),
+    miLibroFavorito: {
+        titulo: "IT",
+        autor: "Stephen King",
+        fechaDeLanzamiento: new Date(oct , 1987)
+    }
+}
+console.log("misDatos");
