0f6a29f78fc40d43d50457ae7a26800744710772
TonyDev92 <tonydeveloper92@gmail.com>
2023-01-30 12:59:09 +0100

    Ejercicio 4 de JavaScript

---
+++ cadenasdetexto.js
@@ -0,0 +1,24 @@
+const nombre = "Tony";
+const apellido = "Carrasco";
+const estudiante = nombre.concat(" ",apellido);
+
+const estudianteMayus = estudiante.toUpperCase();
+
+const estudianteMin = estudiante.toLowerCase();
+
+let estudianteLong = estudiante.length;
+
+let primerCaracter = nombre.charAt(0);
+
+console.log(apellido.indexOf("o"));
+let ultimoCaracter = apellido.charAt(7);
+
+const sinEspacio = nombre + apellido;
+
+let localizador = estudiante.startsWith("Tony");
+
+
+
+
+
+
