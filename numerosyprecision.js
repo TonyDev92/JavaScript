9a078219e429fd6f8e68083082736b0ed0a9eb4f
TonyDev92 <tonydeveloper92@gmail.com>
2023-01-30 18:45:42 +0100

    Ejercicio 5 JavaScript

---
+++ numerosyprecision.js
@@ -0,0 +1,13 @@
+let altura = 170;
+let alturaEnMetros = 1.70;
+let pesoKg = 60.32;
+
+let alturaRedondeada = Math.ceil(alturaEnMetros);
+
+let pesoRedondeado = Math.floor(pesoKg);
+
+let max = Number.MAX_VALUE + 1 == Number.MAX_VALUE;
+
+
+
+
