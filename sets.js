3c7aa706cb6ab5acd1ac5d6cf9afa321c2848238
TonyDev92 <tonydeveloper92@gmail.com>
2023-02-03 17:27:57 +0100

    Ejercicio 7 Javascript

---
+++ sets.js
@@ -0,0 +1,9 @@
+const familia = ["Mayuko","Maria","Mary","Esmeralda","Nuria","Antonio","Tony"];
+const nombresFamilia = new Set(familia);
+
+nombresFamilia.add("Tony");
+
+nombresFamilia.add("JavaScript");
+
+
+
